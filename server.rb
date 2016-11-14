6a905dc68148118c9ddb390fde3b476684d84f11
Ruhmen <RomeoMacBookPro@Romeos-MacBook-Pro.local>
2016-11-14 19:05:08 +0100

    tidy up in class

--- server.rb
+++ server.rb
@@ -5,13 +5,15 @@ require 'open-uri'
 require 'chartkick'
 
 get '/' do
+  @title = "home"
   erb :home
 end
+
 get '/palmi' do
   @url_content  = open('https://api.koubachi.com/v2/plants.json?user_credentials=LFAW3KZO9Rk_xCkxnCEg&app_key=KLAB9OTEKTDF0BO70G9GJWK7') {|f| f.read }
   @json_obj = JSON.parse(@url_content, object_class: OpenStruct)[0]
   @plantdate = @json_obj.plant.created_at
-  @titel = @json_obj.plant.name
   @water_level = @json_obj.plant.vdm_water_level
+  @title = "palmi"
   erb :palmi
 end
