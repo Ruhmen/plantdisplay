a511e1064e9ff05cc5a50f1b920db050a44ed04a
Ruhmen <RomeoMacBookPro@Romeos-MacBook-Pro.local>
2016-11-14 14:00:01 +0100

    add palm

--- server.rb
+++ server.rb
@@ -23,3 +23,11 @@ get '/' do
   @water_level = @json_obj.plant.vdm_water_level
   erb :home
 end
+get '/palmi' do
+  @url_content  = open('https://api.koubachi.com/v2/plants.json?user_credentials=LFAW3KZO9Rk_xCkxnCEg&app_key=KLAB9OTEKTDF0BO70G9GJWK7') {|f| f.read }
+  @json_obj = JSON.parse(@url_content, object_class: OpenStruct)[0]
+  @plantdate = @json_obj.plant.created_at
+  @titel = @json_obj.plant.name
+  @water_level = @json_obj.plant.vdm_water_level
+  erb :palmi
+end
